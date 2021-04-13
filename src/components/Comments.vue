315f52f48e69e416045300a4743cc01a1e3a66c6
unknown <penelathalisson@gmail.com>
2021-04-12 21:01:17 -0300

    first commit

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,40 +1,21 @@
 <template>
   <div class="container">
-    <h1>Comentários</h1>
+    <h1>Threads</h1>
+
     <hr />
-    <div class="form-todo form-gourp">
-      <form @submit.prevent="addComment">
-        <p>
-          <input
-            placeholder="Nome"
-            type="text"
-            name="author"
-            class="form-control"
-            v-model="name"
-            required
-          />
-        </p>
-        <p>
-          <textarea
-            placeholder="Comentário"
-            name="message"
-            class="form-control"
-            v-model="message"
-            required
-          ></textarea>
-        </p>
-        <button type="submit" class="btn btn-primary">Comentar</button>
-      </form>
-    </div>
+    <FormTodo v-on:add-todo="addComment"></FormTodo>
+    <h1>Replies:</h1>
     <div class="list-group">
       <div
         class="list-group-item"
         v-for="(Comments, index) in comments"
         v-bind:key="index"
       >
-        <span class="comments_author"
-          >Autor: <strong>{{ Comments.name }}</strong></span
+        <span class="comments_author">
+          <strong>{{ Comments.name }}</strong
+          >:</span
         >
+        <p class="ml-5 ml-lg-0"></p>
         <p>{{ Comments.message }}</p>
         <div>
           <a href="#" title="Excluir" v-on:click="removeComment(index)"
@@ -42,26 +23,28 @@
           >
         </div>
       </div>
+
+      <div class="row mx-md-n5">
+        <div class="col py-3 px-md-5 bordered col-example"></div>
+        <div class="col py-3 px-md-5 bordered col-example"></div>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import FormTodo from "./FormTodo";
 export default {
+  components: {
+    FormTodo,
+  },
   data() {
     return {
       comments: [],
-      name: "",
-      message: "",
     };
   },
   methods: {
-    addComment() {
-      this.comments.push({
-        name: this.name,
-        message: this.message,
-      });
-      this.name = "";
-      this.message = "";
+    addComment(comment) {
+      this.comments.push(comment);
     },
     removeComment(index) {
       this.comments.splice(index, 1);
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="form-todo form-gourp">
+    <form @submit.prevent="addComment">
+      <p>
+        <input
+          placeholder="Name:"
+          type="text"
+          name="author"
+          class="form-control"
+          v-model="name"
+          required
+        />
+      </p>
+      <p>
+        <textarea
+          placeholder="Here is your answer:"
+          name="message"
+          class="form-control"
+          v-model="message"
+          required
+        ></textarea>
+      </p>
+
+      <button type="submit" class="btn btn-primary">Send</button>
+      <div class="row mx-md-n5">
+        <div class="col py-3 px-md-5 bordered col-example"></div>
+        <div class="col py-3 px-md-5 bordered col-example"></div>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      message: "",
+    };
+  },
+  methods: {
+    addComment() {
+      this.$emit("add-todo", {
+        name: this.name,
+        message: this.message,
+      });
+      this.name = "";
+      this.message = "";
+    },
+  },
+};
+</script>
